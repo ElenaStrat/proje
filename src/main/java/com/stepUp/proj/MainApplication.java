e64b70464eee2a91753e87d1f870d6bd35739114
Elena Chebotnikova <chebotnikovaea@gmail.com>
2025-10-07 07:41:10 +0300

    added Morning

--- src/main/java/com/stepUp/proj/MainApplication.java
+++ src/main/java/com/stepUp/proj/MainApplication.java
@@ -8,5 +8,6 @@ public class MainApplication {
         System.out.println("111111");
         System.out.println("456");
         System.out.println("000");
+        System.out.println("Morning!");
     }
 }
