72502de77186aa825b973dc8ed0fd1ca1fa8ebc4
Elena Chebotnikova <chebotnikovaea@gmail.com>
2025-10-07 07:16:23 +0300

    я второй разработчик и чего-то улучшил по своему усмотрению

--- src/main/java/com/stepUp/proj/MainApplication.java
+++ src/main/java/com/stepUp/proj/MainApplication.java
@@ -2,6 +2,8 @@ package com.stepUp.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("----");
+        System.out.println("Hello World!!!");
         System.out.println("Hello World");
         System.out.println("111111");
         System.out.println("456");
